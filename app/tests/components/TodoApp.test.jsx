f91ae61c33840cd89ddf99134c6f8c9b59b09db5
agolumbovsky <agolumbovsky@gmail.com>
2017-02-08 20:48:30 -0500

    added moment and timestatmps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -17,21 +17,24 @@ describe('TodoApp', () => {
 
         todoApp.setState({todos: []});
         todoApp.handleAddTodo(todoText);
-
         expect(todoApp.state.todos[0].text).toBe(todoText);
+        expect(todoApp.state.todos[0].createAt).toBeA('number');
     });
 
-    it('should toggle completed value when handleToggle called', () => {
+    it('should toggle todo from completed to incompleted', () => {
         var todoData = {
             id: 11,
             text: "test features", 
-            completed: false
+            completed: false,
+            createdAt: 0,
+            completedAt: 123
         }
         var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
         todoApp.setState({todos: [todoData]});
 
-        expect(todoApp.state.todos[0].completed).toBe(false);
-        todoApp.handleToggle(11);
         expect(todoApp.state.todos[0].completed).toBe(true);
+        todoApp.handleToggle(11);
+        expect(todoApp.state.todos[0].completed).toBe(false);
+        expect(todoApp.state.todos[0].completedAt).toNotExist();
     });
 });
